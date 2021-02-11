437cc0daebd131c16d189b1159ab578c9b21f42f
agileassociates <agileassociates@agileastessMini.lan>
2021-02-10 20:57:21 -0500

    first commit

---
+++ 100_Days_of_Swift.playground/Contents.swift
@@ -0,0 +1,44 @@
+import UIKit
+
+var str = "Goodbye"
+var pop = 10_000_000
+
+
+// Multi-line Strings
+
+var str1 = """
+This hits
+over many
+multiple
+lines...
+"""
+
+var str2 = """
+This hits \
+over many \
+multiple \
+lines....
+"""
+
+print(str1, "\n")
+print(str2)
+
+
+
+
+// Doubles & Booleans
+
+var pi = 3.141  // automatically double
+
+var awesome = true
+
+
+
+
+
+
+
+
+
+
+
